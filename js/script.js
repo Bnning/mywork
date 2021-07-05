a0fb7be53b154d510312429a717f5be1dd9d5295
최민영 <>
2021-07-05 16:22:48 +0900

    flie setting

--- js/script.js
+++ js/script.js
@@ -2,13 +2,13 @@
 
     var controller = new ScrollMagic.Controller();
        var wipeAnimation = new TimelineMax()
-      .fromTo(".two", 1, {x:"-100%"}, {x:"0%"} )
+      .fromTo(".two", 1, {y:"-100%"}, {y:"0%"} )
       .fromTo(".three", 1, {y:"-100%"}, {y:"0%"} )
-      .fromTo(".four", 1, {x:"100%"}, {x:"0%"} )
+      .fromTo(".four", 1, {y:"-100%"}, {y:"0%"} )
   
       var scene = new ScrollMagic.Scene({
                   triggerElement: "#container",
-                  triggerHook: "onLeave",
+                  triggerHook: "2",
                   duration: "500%" //이 값이 클 수록 천천히 덮어씀
       })
       .setPin("#container")
@@ -16,5 +16,4 @@
       .addIndicators() 
       .addTo(controller);
       
-  
   }())
